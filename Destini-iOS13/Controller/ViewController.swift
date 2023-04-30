76b68e5b9ffa9c110f5e2246c837fbdfc337156f
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-01 00:26:00 +0300

    Link up stories. When the user turns left, they see a tiger (story1) and when they turn right, they find a chest (story2).

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,15 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0="You see a fork in the road."
-    let choice1="Take a left."
-    let choice2="Take a right."
+    var currentQuestionIndex=0
+    
+    func updateUI(){
+        storyLabel.text=stories[currentQuestionIndex].title
+        choice1Button.setTitle(stories[currentQuestionIndex].choice1, for: .normal)
+        choice2Button.setTitle(stories[currentQuestionIndex].choice2, for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let answer=sender.currentTitle
+        
+        if(answer==stories[0].choice1){
+            currentQuestionIndex=1
+        }else{
+            currentQuestionIndex=2
+        }
+        
+        updateUI()
+    }
+    let stories=[Story(title:"You see a fork in the road.", choice1:"Take a left.", choice2:"Take a right."),Story(title:"You see a tiger.", choice1:"Shout for help.", choice2:"Play dead."),Story(title:"You find a treasure chest", choice1:"Open it.", choice2:"Check for traps.")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text=story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+       updateUI()
 
     }
 
