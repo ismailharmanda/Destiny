1bc6358790289886ca45a3ab99ca95dd328e3f2b
İsmail Harmanda <iharmanda@hotmail.com>
2023-04-30 23:53:26 +0300

    change story on viewLoad

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0="You see a fork in the road."
+    let choice1="Take a left."
+    let choice2="Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text=story0
 
     }
 
