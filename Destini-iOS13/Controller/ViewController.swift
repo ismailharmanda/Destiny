3254af5676ad2ca5146063c6745b7700aa70bc42
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-01 00:50:30 +0300

    Apply MVC

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,26 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain=StoryBrain()
+    
     var currentQuestionIndex=0
     
     func updateUI(){
-        storyLabel.text=stories[currentQuestionIndex].title
-        choice1Button.setTitle(stories[currentQuestionIndex].choice1, for: .normal)
-        choice2Button.setTitle(stories[currentQuestionIndex].choice2, for: .normal)
+        storyLabel.text=storyBrain.getStory().title
+        choice1Button.setTitle(storyBrain.getStory().choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.getStory().choice2, for: .normal)
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
-        let answer=sender.currentTitle
+        let answer=sender.currentTitle!
         
-        if(answer==stories[0].choice1){
-            currentQuestionIndex=1
-        }else{
-            currentQuestionIndex=2
-        }
+        storyBrain.nextStory(userChoice: answer)
         
         updateUI()
     }
-    let stories=[Story(title:"You see a fork in the road.", choice1:"Take a left.", choice2:"Take a right."),Story(title:"You see a tiger.", choice1:"Shout for help.", choice2:"Play dead."),Story(title:"You find a treasure chest", choice1:"Open it.", choice2:"Check for traps.")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -9,3 +9,23 @@
 import Foundation
 
 
+struct StoryBrain{
+    
+    let stories=[Story(title:"You see a fork in the road.", choice1:"Take a left.", choice2:"Take a right."),Story(title:"You see a tiger.", choice1:"Shout for help.", choice2:"Play dead."),Story(title:"You find a treasure chest", choice1:"Open it.", choice2:"Check for traps.")]
+    
+    var currentStoryIndex=0
+    
+    mutating func nextStory(userChoice:String){
+        if(userChoice==stories[0].choice1){
+            currentStoryIndex=1
+        }else{
+            currentStoryIndex=2
+        }
+    }
+    
+    func getStory()->Story{
+       return stories[currentStoryIndex]
+    }
+    
+    
+}
